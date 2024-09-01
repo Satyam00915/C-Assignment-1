c91010fbb9346d30f1afb573d80b62d163fad391
Satyam Upadhyay <satyamupadhyay00915@gmail.com>
2024-09-01 12:28:05 +0530

    assigment 1 done

---
+++ Assignment.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  // Question 1
+  string str = "Hello";
+  string str1 = "World";
+
+  cout << str << str1;
+
+  // Question 2
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
+
+  float ans = (a + b + c + d + e) / 5.0;
+  cout << "The average grade is " << ans;
+
+  // Question 3
+
+  int n;
+  cin >> n;
+  cout << n * n;
+}
