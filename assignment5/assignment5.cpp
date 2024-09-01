db6c828e1f38b2132f30d0e9b0e0fda0e6272f1b
Satyam Upadhyay <satyamupadhyay00915@gmail.com>
2024-09-01 14:19:07 +0530

    done

--- assignment5/assignment5.cpp
+++ assignment5/assignment5.cpp
@@ -4,50 +4,50 @@ using namespace std;
 
 int main() {
   //   // Question 1
-  //   int n;
-  //   cout << "Enter the size of the array: ";
-  //   cin >> n;
-  //   int arr[n];
+  int n;
+  cout << "Enter the size of the array: ";
+  cin >> n;
+  int arr[n];
 
-  //   cout << "Enter the Array: ";
+  cout << "Enter the Array: ";
 
-  //   for (int i = 0; i < 5; i++) {
-  //     cin >> arr[i];
-  //   }
+  for (int i = 0; i < 5; i++) {
+    cin >> arr[i];
+  }
 
-  //   int sum = 0;
+  int sum = 0;
 
-  //   for (int i = 0; i < 5; i++) {
-  //     sum += arr[i];
-  //   }
+  for (int i = 0; i < 5; i++) {
+    sum += arr[i];
+  }
 
-  //   cout << "The sum of array is: " << sum;
+  cout << "The sum of array is: " << sum;
 
   // Question 2
-  //   int n;
-  //   cout << "Enter the size of the array: ";
-  //   cin >> n;
-  //   int arr[n];
+  int n;
+  cout << "Enter the size of the array: ";
+  cin >> n;
+  int arr[n];
 
-  //   cout << "Enter the Array: ";
+  cout << "Enter the Array: ";
 
-  //   for (int i = 0; i < 5; i++) {
-  //     cin >> arr[i];
-  //   }
+  for (int i = 0; i < 5; i++) {
+    cin >> arr[i];
+  }
 
-  //   int oddsum = 0;
-  //   int evensum = 0;
+  int oddsum = 0;
+  int evensum = 0;
 
-  //   for (int i = 0; i < n; i++) {
-  //     if (arr[i] % 2 == 0) {
-  //       evensum += arr[i];
-  //     } else {
-  //       oddsum += arr[i];
-  //     }
-  //   }
+  for (int i = 0; i < n; i++) {
+    if (arr[i] % 2 == 0) {
+      evensum += arr[i];
+    } else {
+      oddsum += arr[i];
+    }
+  }
 
-  //   cout << "The Sum of even numbers is: " << evensum;
-  //   cout << "\nThe Sum of odd numbers is: " << oddsum;
+  cout << "The Sum of even numbers is: " << evensum;
+  cout << "\nThe Sum of odd numbers is: " << oddsum;
 
   // Question 3
 
